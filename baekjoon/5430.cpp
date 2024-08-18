cb977b8f8ca328cde8a8e014ccf961092980e967
hyeunkim at home <hyeunkim@student.42seoul.kr>
2024-08-18 18:42:28 +0900

    dump

--- baekjoon/5430.cpp
+++ baekjoon/5430.cpp
@@ -2,51 +2,57 @@
 
 using namespace std;
 
-deque<int> dq;
-
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
-	int t; cin >> t;
+	int t;
+	cin >> t;
 	while (t--) {
-		string p; cin >> p;
-		int n; cin >> n;
-		string tmp; cin >> tmp;
-		for (int i = 0; i < tmp.length(); i++) {
-			if (tmp[i] == ',' || tmp[i] == '[' || tmp[i] == ']')
-				continue ;
-			tmp.push_back(tmp[i] - '0');
-		}
-		int dir = 1;
-		for (int i = 0; i < p.length(); i++) {
-			if (p[i] == 'R') {
-				dir *= -1;
+		string p, arr;
+		int n;
+		bool reverse = false;
+		bool error = false;
+		deque<int> dq;
+		cin >> p >> n >> arr;
+		for (int i = 0; i < arr.length(); i++) {
+			int tmp = 0;
+			if (isdigit(arr[i])) {
+				while (isdigit(arr[i])) {
+					tmp = tmp * 10 + arr[i] - '0';
+					i++;
+				}
+				dq.push_back(tmp);
 			}
+		}
+		for (int i = 0; i < p.length(); i++) {
+			if (p[i] == 'R')
+				reverse = reverse? false : true;
 			else {
-				if (!dq.size())
-					break ;
-				if (dir < 0) {
+				if (dq.size() == 0)
+					error = true;
+				if (reverse == false)
+					dq.pop_front();
+				else
+					dq.pop_back();
+			}
+		}
+		if (error == true)
+			cout << "error\n";
+		else {
+			cout << '[';
+			while (dq.size() > 0) {
+				if (reverse == true) {
+					cout << dq.back();
 					dq.pop_back();
 				}
-				else
+				else {
+					cout << dq.front();
 					dq.pop_front();
+				}
+				if (dq.size())
+					cout << ',';
 			}
+			cout << "]\n";
 		}
-		if (!dq.size())
-			continue ;
-		cout << '[';
-		if (dir < 0) {
-			while (dq.size()) {
-				cout << dq.back() << ',';
-				dq.pop_back();
-			}
-		}
-		else {
-			while (dq.size()) {
-				cout << dq.front() << ',';
-				dq.pop_front();
-			}
-		}
-		cout << "]\n";
 	}
 }
